c003375a7c0e9d80aaec9d4b995940fe4bc4ac55
henry lay <henry88lay@gmail.com>
2020-02-06 17:28:31 +1100

    added a string sentence and a prompt to enter a integer value

--- chapter-1/c2f.rb
+++ chapter-1/c2f.rb
@@ -1,4 +1,6 @@
-celsius = 100
+puts "Hi there! Please enter Celsius value: "
+celsius = gets.to_i
 fahrenheit = (celsius * 9 / 5) + 32
-puts "The result is #{fahrenheit}"
+print "The Fahrenheit equivalent is "
+print fahrenheit
 puts "."
